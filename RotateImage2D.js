cc15529f48a97bb9af75e291adc6371be7c9958e
NiladriChatterje <radongtxgpu@gmail.com>
2024-05-18 03:10:11 +0530

    rotating 2D image [recognise pattern :)]

---
+++ RotateImage2D.js
@@ -0,0 +1,30 @@
+const rotate = function (matrix) {
+    let swap;
+    for (let i = 0; i < Math.floor(matrix.length / 2); i++) {
+        swap = matrix[i];
+        matrix[i] = matrix[matrix.length - 1 - i];
+        matrix[matrix.length - 1 - i] = swap;
+    }
+    let temp
+    for (let i = 0; i < matrix.length; i++)
+        for (let j = i; j < matrix.length; j++) {
+            temp = matrix[i][j];
+
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp
+        }
+
+
+    return matrix;
+};
+
+console.log(rotate([
+    [5, 1, 9, 11],
+    [2, 4, 8, 10],
+    [13, 3, 6, 7],
+    [15, 14, 12, 16]]))
+
+console.log(rotate([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]]))
