827b10f87c75384ec04ce97b355f59e3e3903267
NiladriChatterje <radongtxgpu@gmail.com>
2024-05-23 22:33:51 +0530

    MergeInterval finally resolved and submitted :)

--- MergeInterval.js
+++ MergeInterval.js
@@ -1,11 +1,16 @@
 const merge = function (intervals) {
+    intervals.sort((a, b) => a[0] - b[0]);
+
     const result = [];
     const coincide = [intervals[0][0], intervals[0][1]];
+
     for (let i = 1; i < intervals.length; i++) {
         const [a, b] = intervals[i];
 
-        if (coincide[1] > a)
-            coincide[1] = b;
+        if (coincide[1] >= a) {
+            coincide[0] = a <= coincide[0] ? a : coincide[0];
+            coincide[1] = b >= coincide[1] ? b : coincide[1];
+        }
         else {
             result.push([...coincide]);
             coincide[0] = a;
@@ -13,7 +18,8 @@ const merge = function (intervals) {
         }
     }
     const res_last = result.length - 1
-    if (result[res_last][0] !== coincide[0] ||
+
+    if (result.length === 0 || result[res_last][0] !== coincide[0] ||
         result[res_last][1] !== coincide[1])
         result.push(coincide)
 
@@ -22,3 +28,4 @@ const merge = function (intervals) {
 
 console.log(merge([[1, 3], [2, 6], [8, 10], [15, 18]]))
 console.log(merge([[1, 4], [4, 5]]))
+console.log(merge([[2, 3], [4, 5], [6, 7], [8, 9], [1, 10]]))
