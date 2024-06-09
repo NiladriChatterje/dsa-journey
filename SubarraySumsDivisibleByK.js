d938b01f8e2ba046e1e8b1046ed5aff084a6d66d
NiladriChatterje <radongtxgpu@gmail.com>
2024-06-09 15:57:40 +0530

    a little more optimization

--- SubarraySumsDivisibleByK.js
+++ SubarraySumsDivisibleByK.js
@@ -1,21 +1,24 @@
 `leetcode 974`
-var subarraysDivByK = function (nums, k) {
-    if (nums.length === 1) return nums[0] % k === 0 ? 1 : 0
+const subarraysDivByK = function (nums, k) {
+
     const map = new Map();
     let sum = 0;
     let count = 0;
+    let mod = 0;
     map.set(sum, 1);
     for (let i of nums) {
         sum += i;
+        mod = sum % k;
+        mod = mod < 0 ? mod + k : mod;
+        if (map.has(mod))
+            count += map.get(mod) ?? 1;
 
-        if (sum % k === 0 || map.has(Math.abs(sum % k)))
-            count += map.get(sum % k) ?? 1;
-
-        map.set(Math.abs(sum % k), (map.get(Math.abs(sum % k)) ?? 0) + 1)
+        map.set(mod, (map.get(mod) ?? 0) + 1)
     }
     return count;
 };
 
 console.log(subarraysDivByK([4, 5, 0, -2, -3, 1], 5))
 console.log(subarraysDivByK([-1, 2, 9], 2))
+console.log(subarraysDivByK([2, -2, 2, -4], 6))
 // console.log(subarraysDivByK([4, 5, 0, -2, -3, 1], 5))
