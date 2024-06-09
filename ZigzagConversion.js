82fbabfcab4a95a80ac4836b7c3b3133ed67c2f3
NiladriChatterje <radongtxgpu@gmail.com>
2024-06-09 17:03:28 +0530

    TCS second question of 15th Jan 2024

--- ZigzagConversion.js
+++ ZigzagConversion.js
@@ -4,15 +4,13 @@ var convert = function (s, numRows) {
     let flag = true;
     let j = 0;
     for (let i of s) {
-        if (result[j] !== undefined)
-            result[j][result[j].length] = i
-        else result[j] = [i];
+        result[j] = result[j] !== undefined ? result[j] + i : i
         j = flag ? j + 1 : j - 1;
         if (j === numRows - 1) flag = false;
         else if (j === 0) flag = true;
     }
 
-    return result.map(item => item.join('')).join('')
+    return result.join('')
 };
 
 console.log(convert("PAYPALISHIRING", 3))
