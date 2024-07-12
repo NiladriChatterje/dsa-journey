ba5938ac6e0b23247e62233a7d2362b0674e7356
NiladriChatterje <radongtxgpu@gmail.com>
2024-07-13 04:00:27 +0530

    took a almost a day to figure out the bug why the versions are not versioning

--- LinkedListSum.js
+++ LinkedListSum.js
@@ -1,59 +1,42 @@
 class ListNode {
-    constructor(val, next,carry) {
+    constructor(val, next) {
         this.val = (val === undefined ? 0 : val);
         this.next = (next === undefined ? null : next);
-        this.carry = (carry === undefined? 0 : carry);
     }
 }
 
-var length = (L) =>{
-    let temp = L;
-let length = 0;
-while(L){
-    length++;
-    L = L.next;
-}
-return length;
-}
+var addTwoNumbers = function (l1, l2) {
+    if (l1 === null || l2 === null) return null;
+    let head = new ListNode((l1.val + l2.val) % 10);
+    let carry = parseInt((l1.val + l2.val) / 10)
+    let temp = head;
+    l1 = l1.next;
+    l2 = l2.next;
 
- var addTwoNumbers = function(l1, l2) {
-    let addedList = new ListNode();
-    let c =0;
-    let tail=addedList;
-    let head = addedList;
-    while(length(l1)>length(l2)?l1:l2){ 
-        if(length(l1)>length(l2) && (l2.next === null)){
-            l2.next = new ListNode();
-        }
-        if(length(l1)<length(l2) && (l1.next === null)){
-            l1.next = new ListNode();
-        }
-        
-        addedList.val = (l1.val + l2.val+addedList.carry)%10;
-        
-        if(length(l1)>length(l2)?l1:l2.next !== null)
-        {
-            addedList.next = new ListNode(undefined,undefined,addedList.carry);
-            addedList.next.carry = Math.trunc((l1.val + l2.val+addedList.carry)/10);
-            tail = tail.next;
-        }
-        c=Math.trunc((l1.val + l2.val+addedList.carry)/10);
-        addedList = addedList.next;
+    while (l1 !== null && l2 !== null) {
+        temp.next = new ListNode((l1.val + l2.val + carry) % 10);
+        carry = parseInt((l1.val + l2.val + carry) / 10);
+        temp = temp.next;
         l1 = l1.next;
         l2 = l2.next;
     }
 
-    if(c !== 0)
-       tail.next = new ListNode(c);
+    while (l1 !== null) {
+        temp.next = new ListNode((l1.val + carry) % 10);
+        carry = parseInt((l1.val + carry) / 10);
+        temp = temp.next;
+        l1 = l1.next;
+    }
+
+    while (l2 !== null) {
+        temp.next = new ListNode((l2.val + carry) % 10);
+        carry = parseInt((l2.val + carry) / 10);
+        temp = temp.next;
+        l2 = l2.next;
+    }
+
+    if (carry !== 0)
+        temp.next = new ListNode(carry);
 
     return head;
 };
-
-l1 = new ListNode(9,new ListNode(9,new ListNode(2)));
-l2 = new ListNode(9,new ListNode(9,new ListNode(4)));
-var checking = addTwoNumbers(l1,l2)
-
-while(checking){
-    console.log(checking.val);
-    checking = checking.next;
-}
--- MaximumScoreFromRemovingSubstring.js
+++ MaximumScoreFromRemovingSubstring.js
@@ -1,3 +1,35 @@
-var maximumGain = function (s, x, y) {
 
+var maximumGain = function (s, x, y) {
+    let min = Math.min(x, y);
+    let a = 0;
+    let b = 0;
+    let score = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        let c = s.charAt(i);
+        if (c > 'b') {
+            score += Math.min(a, b) * min;
+            a = 0;
+            b = 0;
+        } else if (c === 'a') {
+            if (x < y && b > 0) {
+                b--;
+                score += y;
+            } else
+                a++;
+
+        } else if (c === 'b') {
+            if (x > y && a > 0) {
+                a--;
+                score += x;
+            } else
+                b++;
+
+        }
+    }
+
+    score += Math.min(a, b) * min;
+    return score;
 };
+console.log(maximumGain("cdbcbbaaabab", 4, 5))
+console.log(maximumGain("aabbaaxybbaabb", 5, 4))
