21cf10d79631cf036e29ac8f3f16d99bbb1b3663
Niladri Chatterjee <107443816+NiladriChatterje@users.noreply.github.com>
2024-07-19 12:10:12 +0530

    Time: 55 ms (79%), Space: 55.3 MB (5%) - LeetHub

--- 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.js
+++ 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.js
@@ -1,9 +1,38 @@
-var luckyNumbers  = function(matrix) {
-    for(let i = 0; i < matrix.length; i++) {
-        let minNumber = Math.min(...matrix[i]), columnIndex = matrix[i].indexOf(minNumber);
+var luckyNumbers = function (matrix) {
+    const result = [];
+    let max, min;
+    const maxFromColumns = [];
+    const minFromRows = [];
+    let j, k;
+    let i = 0, l = 0;
+    while (i < matrix[0].length || l < matrix.length) {
+        max = 0;
+        min = Infinity
+        j = 0;
+        k = 0;
+        while (j < matrix.length || k < matrix[0].length) {
+            if (j < matrix.length && i < matrix[0].length && max < matrix[j][i])
+                max = matrix[j][i];
 
-        if(matrix.every(arr => arr[columnIndex] <= minNumber)) return [minNumber];
+            if (k < matrix[0].length && l < matrix.length && min > matrix[l][k])
+                min = matrix[l][k];
+
+            j++;
+            k++
+        }
+        maxFromColumns.push(max);
+        minFromRows.push(min)
+        i++;
+        l++
     }
 
-    return [];
+
+    for (let i of maxFromColumns)
+        for (let j of minFromRows)
+            if (i === j)
+                result.push(i);
+
+
+
+    return result;
 };
