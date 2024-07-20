72a10607dad1cd279cb895a006a4a1c964008a42
Niladri Chatterjee <107443816+NiladriChatterje@users.noreply.github.com>
2024-07-20 12:37:16 +0530

    Time: 105 ms (80.25%), Space: 72.6 MB (82.7%) - LeetHub

---
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+var longestCommonSubsequence = function(text1, text2) {
+
+    const length1 = text1.length;
+    const length2 = text2.length
+    const dp = Array.from({ length: length1 + 1 }, () => Array(length2 + 1).fill(0));
+
+    for (let i = 1; i <= length1; ++i) {
+        for (let j = 1; j <= length2; ++j) {
+         
+            if (text1[i - 1] === text2[j - 1]) 
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+             else 
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            
+        }
+    }
+
+    return dp[length1][length2];
+};
