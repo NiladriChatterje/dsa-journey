b5b4ed22473797b731579604bfd463c4cd51a908
Niladri Chatterjee <107443816+NiladriChatterje@users.noreply.github.com>
2024-07-25 20:15:52 +0530

    Time: 186 ms (86.63%), Space: 73.3 MB (47.5%) - LeetHub

---
+++ 0948-sort-an-array/0948-sort-an-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function(nums) {
+    let counting = new Array(2 * 50000 + 1).fill(0);
+    for (let i of nums)
+        counting[i + 50000]++;
+    
+    let writeInd = 0;
+    for (let numberInd = 0; numberInd < counting.length; numberInd++) {
+        let freq = counting[numberInd];
+        while (freq != 0) {
+            nums[writeInd] = numberInd - 50000;
+            writeInd++;
+            freq--;
+        }
+    }
+    return nums;
+};
