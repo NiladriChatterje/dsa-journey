2e8b346f50419cf9b2c70c53b1839040dda19060
Niladri Chatterjee <107443816+NiladriChatterje@users.noreply.github.com>
2024-08-04 11:31:22 +0530

    Time: 63 ms (100%), Space: 55.6 MB (100%) - LeetHub

--- 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.js
+++ 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.js
@@ -1,17 +1,52 @@
-var rangeSum = function (nums, n, left, right) {
-    const result = [];
-    let sum = 0;
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i; j < nums.length; j++) {
-            sum += nums[j];
-            result.push(sum)
-        }
-        sum = 0;
-    }
-    result.sort((a, b) => a - b);
-    sum = 0;
-  for (let i = left - 1; i < right; i++)
-        sum += result[i];
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @param {number} left
+ * @param {number} right
+ * @return {number}
+ */
+var rangeSum = function(nums, n, left, right) {
+    const MODULUS = 1000000007;
 
-    return sum%(10**9+7)
+    function countAndSumSubarrays(array, threshold) {
+        let count = 0;
+        let totalSum = 0n;
+        let currentWindowSum = 0n;
+        let runningSum = 0n;
+        let start = 0;
+
+        for (let end = 0; end < array.length; end++) {
+            runningSum += BigInt(array[end]) * BigInt(end - start + 1);
+            currentWindowSum += BigInt(array[end]);
+            while (currentWindowSum > threshold) {
+                runningSum -= currentWindowSum;
+                currentWindowSum -= BigInt(array[start]);
+                start++;
+            }
+            count += end - start + 1;
+            totalSum += runningSum;
+        }
+        return [count, totalSum];
+    }
+
+    function calculateSumOfFirstKSubarrays(array, k) {
+        let low = BigInt(Math.min(...array));
+        let high = BigInt(array.reduce((a, b) => a + b, 0));
+
+        while (low < high) {
+            let mid = low + (high - low) / 2n;
+            if (countAndSumSubarrays(array, mid)[0] < k) {
+                low = mid + 1n;
+            } else {
+                high = mid;
+            }
+        }
+        const [count, sum] = countAndSumSubarrays(array, low);
+        return sum - low * BigInt(count - k);
+    }
+
+    const result = (calculateSumOfFirstKSubarrays(nums, right) -
+                    calculateSumOfFirstKSubarrays(nums, left - 1) +
+                    BigInt(MODULUS)) % BigInt(MODULUS);
+    return Number(result);
 };
