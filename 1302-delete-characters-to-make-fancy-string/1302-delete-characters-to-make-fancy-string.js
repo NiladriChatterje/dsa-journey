8f383b52dc63e38a2db615250021bb74251e5d4e
Niladri Chatterjee <107443816+NiladriChatterje@users.noreply.github.com>
2025-07-21 08:57:42 +0530

    Time: 90 ms (30.21%), Space: 82.3 MB (11.46%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.js
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.js
@@ -3,14 +3,13 @@
  * @return {string}
  */
 var makeFancyString = function(s) {
-    const result = new Array(s.length);
-    let j = 0;
+    const result = [];
+
     for(let i=0;i<s.length-2;i++){
         if(s.charAt(i)==s.charAt(i+1) && s.charAt(i+1)==s.charAt(i+2))
             continue;
 
-        result[j]=s.charAt(i);
-        j++;
+        result.push(s.charAt(i));
     }
     result.push(s.charAt(s.length-2),s.charAt(s.length-1))
     return result.join("");
