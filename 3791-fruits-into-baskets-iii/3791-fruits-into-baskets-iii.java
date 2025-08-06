3d1168eefacd6c05df710b74d681ec1d7a8c5d19
Niladri Chatterjee <107443816+NiladriChatterje@users.noreply.github.com>
2025-08-06 09:22:45 +0530

    Time: 31 ms (100%), Space: 56.7 MB (100%) - LeetHub

--- 3791-fruits-into-baskets-iii/3791-fruits-into-baskets-iii.java
+++ 3791-fruits-into-baskets-iii/3791-fruits-into-baskets-iii.java
@@ -1,63 +1,36 @@
 class Solution {
     public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
-        List<Integer> sect_mx = new ArrayList<>();
-        int m = baskets.length;
-        int a = (int) Math.sqrt(m); // size of one array
-
-        int cnt = 0, mx = 0;
-        for (int i = 0; i < m; i++) {
-            if (cnt == a) {
-                // creating sector of size a
-                sect_mx.add(mx);
-                mx = baskets[i];
-                cnt = 1;
-            } else {
-                cnt++;
-                mx = Math.max(mx, baskets[i]);
+        int n = baskets.length;
+        int N = 1;
+        while(N <= n) N <<= 1;
+        // Build the segment tree
+        int[] segTree = new int[N << 1];
+        for (int i = 0; i < n; i++)
+        segTree[N + i] = baskets[i];
+        for (int i = N - 1; i > 0; i--) 
+        segTree[i] = Math.max(segTree[2 * i], segTree[2 * i + 1]);
+        int count = 0;
+        for (int i = 0; i < n; ++i) {
+            int x = fruits[i];
+            int index = 1; // Start from the root of the segment tree
+            if (segTree[index] < x) {
+                count++;
+                continue;
+            }
+            // Query the first valid basket
+            while (index < N) {
+                if (segTree[index * 2] >= x) 
+                index *= 2;
+                else
+                index = index * 2 + 1;
+            }
+            // Mark the found basket as used and update the segment tree
+            segTree[index] = -1;
+            while (index > 1) {
+                index >>= 1;
+                segTree[index] = Math.max(segTree[2 * index], segTree[2 * index + 1]);
             }
         }
-
-        // last sector
-        sect_mx.add(mx);
-
-        int remain = 0;
-
-        // start allocating
-        for (int i = 0; i < fruits.length; i++) {
-            int k = 0, set = 1;
-
-            for (int j = 0; j < m; j += a) {
-                if (sect_mx.get(k) < fruits[i]) {
-                    // skip this segment
-                    k++;
-                    continue;
-                }
-
-                // find place to allocate
-                for (int r = j; r < Math.min(j + a, m); r++) {
-                    if (baskets[r] >= fruits[i]) {
-                        set = 0;
-                        baskets[r] = 0;
-                        break;
-                    }
-                }
-
-                // if fruit is allocated in a sector
-                if (set == 0) {
-                    sect_mx.set(k, 0); // find new mx
-                    // update new sector mx
-                    for (int r = j; r < Math.min(j + a, m); r++) {
-                        sect_mx.set(k, Math.max(baskets[r], sect_mx.get(k)));
-                    }
-                    break;
-                }
-
-                k++;
-            }
-
-            remain += set;
-        }
-
-        return remain;
+        return count;     
     }
 }
