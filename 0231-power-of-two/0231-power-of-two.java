00791b9a29f512aa419077e3364bd45e260931fc
Niladri Chatterjee <107443816+NiladriChatterje@users.noreply.github.com>
2025-08-09 13:14:06 +0530

    Time: 4 ms (1.2%), Space: 41 MB (36.36%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n==1) return true;
+        if(n==0) return false;
+        
+        while(n%2==0){
+            n /= 2;
+            System.out.println(n);
+            }
+
+        return n==1;
+    }
+}
