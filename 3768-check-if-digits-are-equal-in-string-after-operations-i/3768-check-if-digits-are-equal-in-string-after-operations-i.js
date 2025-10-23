0ae163361e8aec7c74a58ef2d9f9861a8cb26762
Niladri Chatterjee <107443816+NiladriChatterje@users.noreply.github.com>
2025-10-23 19:40:29 +0530

    Time: 7 ms (85.51%), Space: 61.6 MB (8.69%) - LeetHub

---
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var hasSameDigits = function(s) {
+    const strToDigit = (d)=>{
+        switch(d){
+            case '0': return 0;
+            case '1': return 1;
+            case '2': return 2;
+            case '3': return 3;
+            case '4': return 4;
+            case '5': return 5;
+            case '6': return 6;
+            case '7': return 7;
+            case '8': return 8;
+            case '9': return 9;
+        }
+    }
+
+
+    let temp = s.split("").map(item=>strToDigit(item));
+    let temp2 = [];
+    while(temp.length !== 2){
+     temp2.length = 0;
+     for(let i=0;i<temp.length-1;i++)
+         temp2.push((temp[i]+temp[i+1])%10);
+        
+     [temp,temp2] = [temp2,temp];
+    }
+
+    if(temp[0]==temp[1])
+        return true;
+
+    return false;
+};
