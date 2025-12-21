f988492e5b22f27d958dd068a928d6c3bf4bac1b
Niladri Chatterjee <107443816+NiladriChatterje@users.noreply.github.com>
2025-12-21 10:59:34 +0530

    Time: 1 ms (78.08%), Space: 43.8 MB (20.55%) - LeetHub

--- 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.java
+++ 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.java
@@ -2,30 +2,38 @@ class Solution {
     public int minDeletionSize(String[] strs) {
         int n = strs.length;
         int m = strs[0].length();
-        StringBuilder[] built = new StringBuilder[n];
 
-        for (int i = 0; i < n; i++) 
-            built[i] = new StringBuilder();
-        
+        boolean[] sorted = new boolean[n - 1];
+        int deletions = 0;
 
         for (int col = 0; col < m; col++) {
-            for (int row = 0; row < n; row++) 
-                built[row].append(strs[row].charAt(col));
-            boolean isSorted = true;
-            for (int row = 1; row < n; row++) 
-                if (built[row].toString().compareTo(built[row - 1].toString()) < 0) {
-                    isSorted = false;
-                    break;
-                }
-            
+            boolean needDelete = false;
 
-            if (!isSorted) 
-                for (int row = 0; row < n; row++) 
-                    built[row].deleteCharAt(built[row].length() - 1);
-                
-            
+            // Check if this column breaks order
+            for (int row = 0; row < n - 1; row++) {
+                if (!sorted[row]) {
+                    if (strs[row].charAt(col) > strs[row + 1].charAt(col)) {
+                        needDelete = true;
+                        break;
+                    }
+                }
+            }
+
+            // If invalid, delete this column
+            if (needDelete) {
+                deletions++;
+                continue;
+            }
+
+            // Update sorted status
+            for (int row = 0; row < n - 1; row++) {
+                if (!sorted[row] &&
+                    strs[row].charAt(col) < strs[row + 1].charAt(col)) {
+                    sorted[row] = true;
+                }
+            }
         }
 
-        return m - built[0].length();
+        return deletions;
     }
 }
