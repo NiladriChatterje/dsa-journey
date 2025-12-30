fa85f5f67c20dda99eca50a20cd6a034e0598093
Niladri Chatterjee <107443816+NiladriChatterje@users.noreply.github.com>
2025-12-31 00:21:33 +0530

    Time: 0 ms (100%), Space: 43.3 MB (42.61%) - LeetHub

--- 0840-magic-squares-in-grid/0840-magic-squares-in-grid.java
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.java
@@ -1,43 +1,46 @@
 class Solution {
-    static{
-        try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
-            fw.write("0");
-        } catch (Exception e) {
-        }
-    }
-
     public int numMagicSquaresInside(int[][] grid) {
-        int r = grid.length, c = grid[0].length, ans = 0;
-        if (r < 3 || c < 3) return 0;
-
-        for (int i = 0; i + 2 < r; i++) {
-            for (int j = 0; j + 2 < c; j++) {
-                boolean[] used = new boolean[10];
-                boolean ok = true;
-
-                for (int x = 0; x < 3 && ok; x++) {
-                    for (int y = 0; y < 3; y++) {
-                        int v = grid[i + x][j + y];
-                        if (v < 1 || v > 9 || used[v]) {
-                            ok = false;
-                            break;
-                        }
-                        used[v] = true;
-                    }
+        int n = grid.length;
+        int m = grid[0].length;
+        if (n < 3 || m < 3) {
+            return 0;
+        }
+        int count = 0;
+        for (int i = 0; i <= n - 3; i++) {
+            for (int j = 0; j <= m - 3; j++) {
+                if (isMagic(grid, i, j)) {
+                    count++;
                 }
-                if (!ok) continue;
-
-                int s = grid[i][j] + grid[i][j+1] + grid[i][j+2];
-                for (int x = 0; x < 3; x++)
-                    if (grid[i+x][j] + grid[i+x][j+1] + grid[i+x][j+2] != s) ok = false;
-                for (int y = 0; y < 3; y++)
-                    if (grid[i][j+y] + grid[i+1][j+y] + grid[i+2][j+y] != s) ok = false;
-                if (grid[i][j] + grid[i+1][j+1] + grid[i+2][j+2] != s) ok = false;
-                if (grid[i][j+2] + grid[i+1][j+1] + grid[i+2][j] != s) ok = false;
-
-                if (ok) ans++;
             }
         }
-        return ans;
+        return count;
+    }
+    private boolean isMagic(int[][] grid, int r, int c) {
+        boolean[] seen = new boolean[10];
+        for (int i = r; i < r + 3; i++) {
+            for (int j = c; j < c + 3; j++) {
+                int num = grid[i][j];
+                if (num < 1 || num > 9 || seen[num]) {
+                    return false;
+                }
+                seen[num] = true;
+            }
+        }
+        int sum = grid[r][c] + grid[r][c+1] + grid[r][c+2];
+        for (int i = 0; i < 3; i++) {
+            if (grid[r+i][c] + grid[r+i][c+1] + grid[r+i][c+2] != sum) {
+                return false;
+            }
+            if (grid[r][c+i] + grid[r+1][c+i] + grid[r+2][c+i] != sum) {
+                return false;
+            }
+        }
+        if (grid[r][c] + grid[r+1][c+1] + grid[r+2][c+2] != sum) {
+            return false;
+        }
+        if (grid[r][c+2] + grid[r+1][c+1] + grid[r+2][c] != sum) {
+            return false;
+        }
+        return true;
     }
 }
