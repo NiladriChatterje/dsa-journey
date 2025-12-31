1de31bde87a6ba3b10185725fce1f50ce744a450
Niladri Chatterjee <107443816+NiladriChatterje@users.noreply.github.com>
2025-12-31 20:23:08 +0530

    Time: 13 ms (98.98%), Space: 78.9 MB (65.48%) - LeetHub

--- 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.java
+++ 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.java
@@ -1,59 +1,68 @@
 class Solution {
-    private static final int[][] DIRECTIONS = {{0, 1},{0, -1},{1, 0},{-1, 0}};
-    
     public int latestDayToCross(int row, int col, int[][] cells) {
-        int lo = 0, hi = cells.length - 1;
-        int result = 0;
-        
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (canCross(mid, cells, row, col)) {
-                result = mid;
-                lo = mid + 1;
-            } else 
-                hi = mid - 1;
-        }
-        
-        return result+1;
-    }
-    
-    private boolean canCross(int days, int[][] cells, int row, int col) {
-        boolean[][] flooded = new boolean[row][col];
-        
-        for (int i = 0; i <= days; i++) 
-            flooded[cells[i][0] - 1][cells[i][1] - 1] = true;
-        
-        
-        Queue<int[]> queue = new ArrayDeque<>();
-        boolean[][] visited = new boolean[row][col];
-        
-        for (int j = 0; j < col; j++) 
-            if (!flooded[0][j]) {
-                queue.offer(new int[]{0, j});
-                visited[0][j] = true;
-            }
-        
-        
-        if (queue.isEmpty()) return false;
-        
-        while (!queue.isEmpty()) {
-            int[] curr = queue.poll();
-            int x = curr[0], y = curr[1];
-            
-            if (x == row - 1) return true;
-            
-            for (int[] dir : DIRECTIONS) {
-                int nx = x + dir[0];
-                int ny = y + dir[1];
-                
-                if (nx >= 0 && nx < row && ny >= 0 && ny < col 
-                    && !visited[nx][ny] && !flooded[nx][ny]) {
-                    visited[nx][ny] = true;
-                    queue.offer(new int[]{nx, ny});
+        DSU dsu = new DSU(row * col + 2);
+        int[][] grid = new int[row][col];
+        int[][] dirs = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 }, { 1, 1 }, { 1, -1 }, { -1, 1 }, { -1, -1 } };
+
+        for (int i = 0; i < row * col; i++) {
+            int r = cells[i][0] - 1;
+            int c = cells[i][1] - 1;
+            grid[r][c] = 1;
+
+            int id1 = r * col + c + 1;
+
+            for (int[] d : dirs) {
+                int nr = r + d[0];
+                int nc = c + d[1];
+                if (nr >= 0 && nr < row && nc >= 0 && nc < col && grid[nr][nc] == 1) {
+                    dsu.union(id1, nr * col + nc + 1);
                 }
             }
+
+            if (c == 0)
+                dsu.union(0, id1);
+            if (c == col - 1)
+                dsu.union(row * col + 1, id1);
+
+            if (dsu.find(0) == dsu.find(row * col + 1))
+                return i;
         }
-        
-        return false;
+
+        return -1;
+    }
+}
+
+class DSU {
+    int[] root, size;
+
+    public DSU(int n) {
+        root = new int[n];
+        size = new int[n];
+        for (int i = 0; i < n; i++)
+            root[i] = i;
+        Arrays.fill(size, 1);
+    }
+
+    public int find(int x) {
+        if (root[x] != x)
+            root[x] = find(root[x]);
+        return root[x];
+    }
+
+    public void union(int x, int y) {
+        int rx = find(x);
+        int ry = find(y);
+
+        if (rx == ry)
+            return;
+
+        if (size[rx] > size[ry]) {
+            int tmp = rx;
+            rx = ry;
+            ry = tmp;
+        }
+
+        root[rx] = ry;
+        size[ry] += size[rx];
     }
 }
