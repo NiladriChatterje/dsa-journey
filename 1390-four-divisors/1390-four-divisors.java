7de1ba8712adee5675ad44f7f57cfe7baf564aa5
Niladri Chatterjee <107443816+NiladriChatterje@users.noreply.github.com>
2026-01-04 11:32:03 +0530

    Time: 10 ms (79.55%), Space: 46.4 MB (51.01%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.java
+++ 1390-four-divisors/1390-four-divisors.java
@@ -1,42 +1,65 @@
-class Solution {
-    public int sumFourDivisors(int[] nums) {
-        Map<Integer,Integer> setOfNoHaving4Divisors = new HashMap<>();
-        Set<Integer> set = new HashSet<>();
-        int sum = 0;
-        int j;
-        int count;
-        int tempSum;
+// class Solution {
+//     public int sumFourDivisors(int[] nums) {
+//         Map<Integer,Integer> setOfNoHaving4Divisors = new HashMap<>();
+//         Set<Integer> set = new HashSet<>();
+//         int sum = 0;
+//         int j;
+//         int count;
+//         int tempSum;
 
-        for(int num : nums){
-            if(setOfNoHaving4Divisors.containsKey(num)){
-                sum += setOfNoHaving4Divisors.get(num);
-                continue;
-            }
+//         for(int num : nums){
+//             if(setOfNoHaving4Divisors.containsKey(num)){
+//                 sum += setOfNoHaving4Divisors.get(num);
+//                 continue;
+//             }
 
-            if(!set.add(num))
-                continue;
+//             if(set.contains(num))
+//                 continue;
             
-            j=1;
-            count = 0;
-            tempSum = 0;
-            while(j <= num/2){
-                if(num%j == 0){
-                    tempSum += j;
-                    count++;
-                }
-                if(count >= 4)
-                    break;
-                j++;
+//             j=1;
+//             count = 0;
+//             tempSum = 0;
+//             while(j <= num/2){
+//                 if(num%j == 0){
+//                     tempSum += j;
+//                     count++;
+//                 }
+//                 if(count >= 4)
+//                     break;
+//                 j++;
+//             }
+
+//             if(count == 3){
+//                 setOfNoHaving4Divisors.put(num,tempSum+num);
+//                 sum += tempSum + num;
+//                 }
+//             else
+//                 set.add(num);
+//         }
+
+//         return sum;
+//     }
+// }
+
+class Solution {
+    private int factors(int n) {
+        int sum = 0, c = 0;
+        for(int i = 2; i*i <= n; i++) {
+            if(n % i == 0) {
+                int j = n / i;
+                if(j == i || c > 0) return 0;
+                sum += i + j;
+                c++;
             }
-
-            if(count == 3){
-                setOfNoHaving4Divisors.put(num,tempSum+num);
-                sum += tempSum + num;
-                }
-            else
-                set.add(num);
         }
-
+        if(c == 0) return 0;
+        return 1 + sum + n;
+    }
+    public int sumFourDivisors(int[] nums) {
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++) {
+            sum += factors(nums[i]);
+        }
         return sum;
     }
 }
