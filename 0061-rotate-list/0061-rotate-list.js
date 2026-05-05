dc02c1df80b3237940f7adda3ead20df32494653
Niladri Chatterjee <107443816+NiladriChatterje@users.noreply.github.com>
2026-05-05 23:47:56 +0530

    Time: 1 ms (19.58%), Space: 56.6 MB (67.93%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.js
+++ 0061-rotate-list/0061-rotate-list.js
@@ -13,25 +13,26 @@
 var rotateRight = function(head, k) {
     if (head == null) 
         return null;
-    
+
     if(k==0)
         return head;
 
-    const arr = new Array();
+    const arr = new Array(500);
     let temp = head;
     let length = 0;
-
+    let i = 0
     while(temp!=null){
-        arr.push(temp);
+        arr[i]=(temp);
         length++;
         temp = temp.next;
+        i++;
     }
 
     k = (k)%length;
 
     if(k == 0)
         return head;
-        
+
     let index = length - k - 1;
     arr[length-1].next = arr[0];
     arr[index].next = null;
