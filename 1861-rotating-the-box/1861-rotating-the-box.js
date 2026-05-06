32f989a7fadd495e92cebf556cfae1a2dfeca923
Niladri Chatterjee <107443816+NiladriChatterje@users.noreply.github.com>
2026-05-07 01:17:34 +0530

    Time: 23 ms (53.66%), Space: 82.8 MB (70.73%) - LeetHub

---
+++ 1861-rotating-the-box/1861-rotating-the-box.js
@@ -0,0 +1,43 @@
+/**
+ * @param {character[][]} boxGrid
+ * @return {character[][]}
+ */
+var rotateTheBox = function(boxGrid) {
+    let right;
+    let left;
+
+    for(let box of boxGrid){
+        right = box.length - 1;
+        while(right>=0 && (box[right]=="#" || box[right] == '*'))
+            right--;
+
+        left = right - 1;
+        while(left>=0){
+            if(box[left]=="#"&&box[right]=="."){
+                box[right] = "#";
+                box[left] = ".";
+                right--;
+            }else if(box[left] == "*"){
+                while(left>=0 && (box[left]=="#" || box[left] == '*'))
+                    left--;
+                right = left;
+                }
+
+            left--;
+        }
+    }
+
+    const res = new Array(boxGrid[0].length);
+    let x = 0;
+    while(x < boxGrid[0].length){
+        res[x] = new Array(boxGrid.length);
+        x++;
+    }
+
+    for(let i=0; i<boxGrid.length; i++)
+        for(let j=0; j<boxGrid[0].length;j++)
+            res[j][i] = boxGrid[boxGrid.length-1-i][j]
+            
+    
+    return res
+};
