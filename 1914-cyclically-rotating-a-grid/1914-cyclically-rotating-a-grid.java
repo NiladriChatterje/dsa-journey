14aabad6be0de21aa5c4bd13885a6a82787634c3
Niladri Chatterjee <107443816+NiladriChatterje@users.noreply.github.com>
2026-05-09 14:40:23 +0530

    Time: 0 ms (100%), Space: 47.1 MB (92.54%) - LeetHub

--- 1914-cyclically-rotating-a-grid/1914-cyclically-rotating-a-grid.java
+++ 1914-cyclically-rotating-a-grid/1914-cyclically-rotating-a-grid.java
@@ -1,57 +1,46 @@
 class Solution {
+    private int traverse(int[][] g, int[] la, int m, int n, int i, boolean f, int v, int l) {
+        int r1 = i, c1 = i;
+        int r2 = i, c2 = n - i - 1;
+        int r3 = m - i - 1, c3 = n - i - 1;
+        int r4 = m - i - 1, c4 = i;
+        /*System.out.println(r1 + " " + c1);
+        System.out.println(r2 + " " + c2);
+        System.out.println(r3 + " " + c3);
+        System.out.println(r4 + " " + c4);*/
+        int j = 0;
+        for (int k = r1; k <= r4; k++)
+            if (f)
+                g[k][c1] = la[(j++ + v) % l];
+            else
+                la[j++] = g[k][c1];
+        for (int k = c4 + 1; k <= c3; k++)
+            if (f)
+                g[r4][k] = la[(j++ + v) % l];
+            else
+                la[j++] = g[r4][k];
+        for (int k = r3 - 1; k >= r2; k--)
+            if (f)
+                g[k][c2] = la[(j++ + v) % l];
+            else
+                la[j++] = g[k][c2];
+        for (int k = c2 - 1; k > c1; k--)
+            if (f)
+                g[r1][k] = la[(j++ + v) % l];
+            else
+                la[j++] = g[r1][k];
+        return j;
+    }
+
     public int[][] rotateGrid(int[][] grid, int k) {
-
-        int rows = grid.length;
-        int cols = grid[0].length;
-
-        int totalLayers = Math.min(rows, cols) / 2;
-
-        for(int layer = 0; layer < totalLayers; layer++) {
-
-            ArrayList<Integer> elements = new ArrayList<>();
-
-            int top = layer;
-            int left = layer;
-
-            int bottom = rows - layer - 1;
-            int right = cols - layer - 1;
-
-            for(int col = left; col <= right; col++)
-                elements.add(grid[top][col]);
-
-            for(int row = top + 1; row <= bottom - 1; row++)
-                elements.add(grid[row][right]);
-
-            for(int col = right; col >= left; col--)
-                elements.add(grid[bottom][col]);
-
-            for(int row = bottom - 1; row >= top + 1; row--)
-                elements.add(grid[row][left]);
-
-            int size = elements.size();
-            int index = k % size;
-
-            for(int col = left; col <= right; col++) {
-                grid[top][col] = elements.get(index);
-                index = (index + 1) % size;
-            }
-
-            for(int row = top + 1; row <= bottom - 1; row++) {
-                grid[row][right] = elements.get(index);
-                index = (index + 1) % size;
-            }
-
-            for(int col = right; col >= left; col--) {
-                grid[bottom][col] = elements.get(index);
-                index = (index + 1) % size;
-            }
-
-            for(int row = bottom - 1; row >= top + 1; row--) {
-                grid[row][left] = elements.get(index);
-                index = (index + 1) % size;
-            }
+        int m = grid.length, n = grid[0].length;
+        int[] la = new int[(m + n - 2) << 1];
+        int rc = Math.min(m, n) >> 1;
+        for (int i = 0; i < rc; i++) {
+            int l = traverse(grid, la, m, n, i, false, k, 0);
+            //System.out.println(l);
+            traverse(grid, la, m, n, i, true, l - (k % l), l);
         }
-
         return grid;
     }
 }
