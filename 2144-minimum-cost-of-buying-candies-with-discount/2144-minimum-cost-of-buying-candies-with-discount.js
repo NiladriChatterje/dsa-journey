94484efd8934d6c12fdbd9bbce60f3245ede5e5a
Niladri Chatterjee <107443816+NiladriChatterje@users.noreply.github.com>
2026-06-01 10:55:51 +0530

    Time: 1 ms (90.48%), Space: 54.9 MB (35.71%) - LeetHub

---
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minimumCost = function(cost) {
+    cost.sort((a,b)=>a-b);
+    let i = cost.length - 1;
+    let sum = 0;
+    while(i>=0){
+        if(i-1>=0)
+            sum += (cost[i]+cost[i-1]);
+        else
+            sum += cost[i];
+        i-=3;
+    }
+
+    return sum;
+};
